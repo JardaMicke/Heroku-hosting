e58b130433a405af501fddad8ecb68ff94c3a251
Jaroslav Micke <Jamicke@seznam.cz>
2019-11-14 16:22:49 +0100

    Hosting-second-app

---
+++ MyWebShop/Controllers/NewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Office.CustomUI;
+using Microsoft.AspNetCore.Mvc;
+using MyWebShop.Models;
+
+namespace MyWebShop.Controllers
+{    
+    [Route("")]
+    public class NewController : Controller
+    {
+        static List<WebShopItems> ItemList = new List<WebShopItems>()
+        {
+            new WebShopItems("Running shoes", "Nike running shoes for every day sport", 1000, 5),
+            new WebShopItems("Printer", "Some HP printer that will print pages", 3000, 2),
+            new WebShopItems("Coca cola", "0,5l standart coke", 25, 0),
+            new WebShopItems("Wokin", "Chicken with fried rice and WOKIN sauce", 119, 10),
+            new WebShopItems("T-shirt", "Blue with c corgi on bike", 300, 1)
+        };
+
+        [Route("")]
+        public IActionResult WebShop()
+        {
+            ViewBag.items = ItemList;
+            return View();
+        }
+
+        [Route("OnlyAvailable")]
+        public IActionResult OnlyAvailable(int quantityOfStock)
+        {
+            WebShopItems itemOutOfStock = ItemList.FirstOrDefault(i => i.QuantityOfStock == 0);
+            ItemList.Remove(itemOutOfStock);
+            return RedirectToAction("WebShop");
+        }
+
+        [Route("ChepestFirst")]
+        public IActionResult ChepestFirst(int price)
+        {
+            ItemList = ItemList.OrderBy(c => c.Price).ToList();
+            return RedirectToAction("WebShop");
+        }
+    }
+}
